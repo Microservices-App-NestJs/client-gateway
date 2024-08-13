5a15414e78ac113936e1da9080452036dc4c0aa1
samuel20468 <136033542+samuel20468@users.noreply.github.com>
2024-08-13 03:06:18 -0500

    "Updated exception filter to handle 'Empty response' error, added try-catch blocks to controllers, removed microservices imports from modules, and modified tsconfig.json - Dockerfile and dockerignore created"

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -22,18 +22,30 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(
-    @Inject(NATS_SERVICE) private readonly client: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.client.send('createOrder', createOrderDto);
+  async create(@Body() createOrderDto: CreateOrderDto) {
+    try {
+      const newOrder = await firstValueFrom(
+        this.client.send('createOrder', createOrderDto),
+      );
+      return newOrder;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Get()
-  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.client.send('findAllOrders', orderPaginationDto);
+  async findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    try {
+      const orders = await firstValueFrom(
+        this.client.send('findAllOrders', orderPaginationDto),
+      );
+      return orders;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Get(':status')
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -34,8 +34,15 @@ export class ProductsController {
   }
 
   @Get()
-  findAll(@Query() paginationDto: PaginationDto) {
-    return this.client.send({ cmd: 'find_all_products' }, paginationDto);
+  async findAll(@Query() paginationDto: PaginationDto) {
+    try {
+      const products = await firstValueFrom(
+        this.client.send({ cmd: 'find_all_products' }, paginationDto),
+      );
+      return products;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Get(':id')
